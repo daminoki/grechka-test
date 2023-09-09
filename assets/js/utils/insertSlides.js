47faaa0dd1831441ab51c4165685720e91b995bd
Daria Andreeva <dd1556@mail.ru>
2023-09-10 00:19:12 +0400

    fix insert slides function

--- assets/js/utils/insertSlides.js
+++ assets/js/utils/insertSlides.js
@@ -1,40 +1,28 @@
 export function insertSlides(slidesArray) {
     return slidesArray.map((slide) => {
-        function getImage() {
-            const imgElement = document.createElement('img');
-            imgElement.className = 'cars__img';
-            imgElement.src = slide.imgUrl;
-            imgElement.alt = slide.title;
-            
-            imgElement.onerror = function() {
-                console.log(1)
-                const errorElement = `
-                    <div class="cars__img-error">
-                        <p class="cars__error-text">ERROR</p>
-                        <img src="./templates/img/error.png" alt="Ошибка загрузки слайда" width="950" height="110">
-                    </div>
-                `;
-
-                imgElement.replaceWith(errorElement);
-            };
-            
-            return imgElement;
+        function getImage () {
+            if (slide.imgUrl) {
+                return `<img class="cars__img" src=${slide.imgUrl} alt=${slide.title}>`
+            } else {
+                return `<div class="cars__img-error">
+                    <p class="cars__error-text">ERROR</p>
+                    <img src="./templates/img/error.png" alt="Ошибка загрузки слайда" width="950" height="110">
+                </div>`
+            }
         }
 
-        return `
-            <div class="cars__slide swiper-slide">
-                <div class="cars__img-container">
-                    ${getImage().outerHTML}
-                </div>
-                <div class="cars__title-wrapper">
-                    <h2 class="cars__title">${slide.title}</h2>
-                </div>
-                <div class="cars__description-wrapper"><p class="cars__description">${slide.desc}</p></div>
-                <div class="cars__like-wrapper">
-                    <button type="button" data-index=${slide.id} class="cars__like-button"></button>
-                    <p class="cars__like-count">like: <span>${slide.likeCnt}</span></p>
-                </div>
+        return `<div class="cars__slide swiper-slide">
+            <div class="cars__img-container">
+                ${getImage()}
             </div>
-        `;
+            <div class=cars__title-wrapper>
+                <h2 class="cars__title">${slide.title}</h2>
+            </div>
+            <div class="cars__description-wrapper"><p class="cars__description">${slide.desc}</p></div>
+            <div class="cars__like-wrapper">
+                <button type="button" data-index=${slide.id} class="cars__like-button"></button>
+                <p class="cars__like-count">like: <span>${slide.likeCnt}</span></p>
+            </div>
+        </div>`;
     });
 }
